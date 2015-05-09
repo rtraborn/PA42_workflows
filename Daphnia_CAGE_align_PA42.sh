f54de41ff00203deee4e1c7c44491eba96bf8428
Taylor Raborn <rtraborn@indiana.edu>
2015-05-09 16:44:52 -0400

    Completed Daphnia Alignment Job Script

--- Daphnia_CAGE_align_PA42.sh
+++ Daphnia_CAGE_align_PA42.sh
@@ -3,7 +3,7 @@
 #PBS -N RTR_ML_batch
 #PBS -k o
 #PBS -q cpu
-#PBS -l nodes=1:ppn=1,vmem=500gb
+#PBS -l nodes=1:ppn=8,vmem=500gb
 #PBS -l walltime=48:00:00
 #PBS -q shared
 #BS -m abe
@@ -11,13 +11,24 @@
 
 
 ulimit -s unlimited
-module load bwa
+module load bwa/0.6.2
 
-GENOME=/home/rtraborn/Daphnia/genome_sequence/DP.fasta
+WD=/N/dc2/projects/Daphnia_Gene_Expression/CAGE_data/demultiplexed/ 
+GENOME=/N/dc2/projects/Daphnia_exon/PA42_assembly/step7_PA42_assembly/PA42_scaffold_1.0.fasta
+
+cd $GENOME
+echo "Indexing Genome..."
+bwa index $GENOME
+
+cd $WD
+
 for FQ in *.fastq
 do
-bwa aln -t6 -B 3 $GENOME -f $(basename $FQ .fastq).sai $FQ
+bwa aln -t8 -B 3 -n 3 $GENOME -f $(basename $FQ .fastq).sai $FQ
 bwa samse $GENOME $(basename $FS .fastq).sai $FQ
     samtools view -uS - |
     samtools sort - $(basename $FQ .fastq)
 done
+
+echo "Job Complete!"
+
