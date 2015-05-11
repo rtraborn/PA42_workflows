dbf45e3726a5677be6a2782e514588346b51a39e
rtraborn <rtraborn@indiana.edu>
2015-05-11 00:13:11 -0400

    Updating Job Script

--- Daphnia_CAGE_align_PA42.sh
+++ Daphnia_CAGE_align_PA42.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#PBS -N RTR_ML_batch
+#PBS -N RTR_PA42_CAGE_align
 #PBS -k o
 #PBS -q cpu
 #PBS -l nodes=1:ppn=8,vmem=500gb
-#PBS -l walltime=30:00:00
+#PBS -l walltime=36:00:00
 #PBS -q shared
 #BS -m abe
 #PBS -M rtraborn@indiana.edu
@@ -12,20 +12,23 @@
 
 ulimit -s unlimited
 module load bwa/0.6.2
+module load samtools
 
-WD=/N/dc2/projects/Daphnia_Gene_Expression/CAGE_data/demultiplexed/ 
-GENOME=/N/dc2/projects/Daphnia_exon/PA42_assembly/step7_PA42_assembly/PA42_scaffold_1.0.fasta
-
-cd $GENOME
-echo "Indexing Genome..."
-bwa index $GENOME
+WD=/N/dc2/projects/Daphnia_Gene_Expression/CAGE_data/demultiplexed/
+GENOME=PA42_scaffold_1.0.fasta
 
 cd $WD
+
+echo "Indexing Genome"
+
+bwa index $GENOME
+
+echo "Aligning the reads"
 
 for FQ in *.fastq
 do
 bwa aln -t8 -B 3 -n 3 $GENOME -f $(basename $FQ .fastq).sai $FQ
-bwa samse $GENOME $(basename $FS .fastq).sai $FQ
+bwa samse $GENOME $(basename $FQ .fastq).sai $FQ
     samtools view -uS - |
     samtools sort - $(basename $FQ .fastq)
 done
---
+++ Genome_Index.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#PBS -N PA42_genome_index
+#PBS -k o
+#PBS -q cpu
+#PBS -l nodes=1:ppn=1,vmem=500gb
+#PBS -l walltime=4:00:00
+#PBS -q shared
+#BS -m abe
+#PBS -M rtraborn@indiana.edu
+
+ulimit -s unlimited
+module load bwa/0.6.2
+
+GENOME=/N/dc2/projects/Daphnia_exon/PA42_assembly/step7_PA42_assembly/PA42_scaffold_1.0.fasta
+
+bwa index $GENOME
+
+echo "Job Complete!"
+
